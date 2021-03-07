efca691cae2457023ca6abc8019027fb9eff8f5a
Sebastian <sebastian-maciel@outlook.com>
2021-03-07 02:44:49 -0600

    Se agrego adapter a la main activity

--- app/src/main/java/sebastian/maciel/popcornfactory/MainActivity.kt
+++ app/src/main/java/sebastian/maciel/popcornfactory/MainActivity.kt
@@ -1,11 +1,124 @@
 package sebastian.maciel.popcornfactory
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_detalle_pelicula.view.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.pelicula.view.*
 
 class MainActivity : AppCompatActivity() {
+    var adapter: PeliculaAdapter? = null
+    var peliculas = ArrayList<Pelicula>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cargarPeliculas()
+
+        adapter = PeliculaAdapter(this, peliculas)
+        grid_view.adapter = adapter
     }
+
+
+    fun cargarPeliculas() {
+        peliculas.add(
+            Pelicula(
+                "Bones",
+                R.drawable.bones,
+                R.drawable.bonesheader,
+                "Dr. Temperance Brennan is a brilliant, but lonely, anthropologist whom is approached by an ambitious FBI agent, named Seely Booth, to help the bureau solve a series of unsolved crimes by identifying the long-dead bodies of missing persons by their bone structure. But both Agent Booth and Dr. Brennan and her team come up again a variety of interference from red tape, corruption, and local noncooperation."
+            )
+        )
+        peliculas.add(
+            Pelicula(
+                "Dr. House",
+                R.drawable.drhouse,
+                R.drawable.househeader,
+                "The series follows the life of anti-social, pain killer addict, witty and arrogant medical doctor Gregory House (Hugh Laurie) with only half a muscle in his right leg. He and his team of medical doctors try to cure complex and rare diseases from very ill ordinary people in the United States of America."
+            )
+        )
+        peliculas.add(
+            Pelicula(
+                "Big hero 6",
+                R.drawable.bighero6,
+                R.drawable.headerbighero6,
+                "When a devastating event befalls the city of San Fransokyo and catapults Hiro into the midst of danger, he turns to Baymax and his close friends adrenaline junkie Go Go Tomago, neatnik Wasabi, chemistry whiz Honey Lemon and fanboy Fred. Determined to uncover the mystery, Hiro transforms his friends into a band of high-tech heroes called &quot;Big Hero 6.&quot;"
+            )
+        )
+        peliculas.add(
+            Pelicula(
+                "Dr. Who",
+                R.drawable.drwho,
+                R.drawable.drwhoheader,
+                "Traveling across time and space, the immortal time-lord known as &#39;The Doctor&#39; travels across the universe with his many companions and his loyal shape-shifting space-ship: The TARDIS. The Doctor faces many threats across many generations: from The Daleks, The Cybermen and his time- lord adversary The Master to the sinister Davros, creator of The Daleks."
+            )
+        )
+        peliculas.add(
+            Pelicula(
+                "Friends",
+                R.drawable.friends,
+                R.drawable.friends,
+                "Rachel Green, Ross Geller, Monica Geller, Joey Tribbiani, Chandler Bing and Phoebe Buffay are six 20 something year-olds, living off of one another in the headert of New York City. Over the course of ten years, this average group of buddies goes through massive mayhem, family trouble, past and future romances, fights, laughs, tears and surprises as they learn what it really means to be a friend."
+            )
+        )
+        peliculas.add(Pelicula("Inception", R.drawable.inception, R.drawable.inceptionheader, ""))
+        peliculas.add(Pelicula("Leap year", R.drawable.leapyear, R.drawable.leapyearheader, ""))
+        peliculas.add(Pelicula("Toy story", R.drawable.toystory, R.drawable.toystoryheader, ""))
+        peliculas.add(Pelicula("Smalville", R.drawable.smallville, R.drawable.smallvilleheader, ""))
+
+
+    }
+}
+
+
+class PeliculaAdapter : BaseAdapter {
+    var peliculas = ArrayList<Pelicula>()
+    var context: Context? = null
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var pelicula = peliculas[position]
+        var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var vista = inflator.inflate(R.layout.pelicula, null)
+        vista.iv_pelicula.setImageResource(pelicula.image)
+        vista.iv_nombre_pelicula.setText(pelicula.titulo)
+
+        vista.iv_pelicula.setOnClickListener(){
+            var intent = Intent(context, DetallePelicula::class.java)
+            intent.putExtra("titulo", pelicula.titulo)
+            intent.putExtra("image", pelicula.image)
+            intent.putExtra("header", pelicula.header)
+            intent.putExtra("sinopsis", pelicula.sinopsis)
+            context!!.startActivity(intent)
+        }
+
+
+        return vista
+    }
+
+    constructor(context: Context, peliculas: ArrayList<Pelicula>) {
+        this.context = context
+        this.peliculas = peliculas
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+
 }
--- app/src/main/java/sebastian/maciel/popcornfactory/Pelicula.kt
+++ app/src/main/java/sebastian/maciel/popcornfactory/Pelicula.kt
@@ -3,7 +3,7 @@ package sebastian.maciel.popcornfactory
 data class Pelicula(
     var titulo: String,
     var image: Int,
-    var hear: Int,
+    var header: Int,
     var sinopsis: String
 ) {
 
